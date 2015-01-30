7af2e9feacc19eaab20273b059fc566260c9e59e
minagaba <minagaba@gmail.com>
2015-01-30 10:45:14 +0200

    xsl

--- HaZera/Form1.Designer.cs
+++ HaZera/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"}, -1);
             this.button1 = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.inputFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(286, 76);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(216, 509);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,21 +62,136 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(472, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(39, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // inputFile
+            // 
+            this.inputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputFile.Location = new System.Drawing.Point(70, 13);
+            this.inputFile.Name = "inputFile";
+            this.inputFile.Size = new System.Drawing.Size(396, 20);
+            this.inputFile.TabIndex = 2;
+            this.inputFile.Text = "D:\\Users\\Haggai\\Documents\\GitHub\\HaZera\\HaZera\\bin\\Debug\\input.xlsx";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Excel file:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView.Location = new System.Drawing.Point(12, 69);
+            this.listView.Name = "listView";
+            this.listView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listView.RightToLeftLayout = true;
+            this.listView.Size = new System.Drawing.Size(499, 405);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "חלקה";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "שקית זרעים";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "זריעה";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "PCR";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "עמידות";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "הערות";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 480);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(496, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 447);
+            this.ClientSize = new System.Drawing.Size(523, 542);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputFile);
+            this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox inputFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ProgressBar progressBar1;
 
     }
 }
--- HaZera/Form1.cs
+++ HaZera/Form1.cs
@@ -7,9 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
 using GAF;
 using GAF.Operators;
 using GAF.Extensions;
+
+//using NPOI.HSSF.Model;
+//using NPOI.HSSF.UserModel;
+using NPOI.XSSF.Model;
+using NPOI.XSSF.UserModel;
+
 
 namespace HaZera
 {
@@ -20,6 +29,20 @@ namespace HaZera
             InitializeComponent();
             PopulateTestPlan();
         }
+
+        public const int FIELD_COLUMN_NUMBER = 0;
+        public const int BAGNAME_COLUMN_NUMBER = 1;
+        public const int SEEDSTOPLANT_COLUMN_NUMBER = 2;
+        public const int SEEDSTOSAMPLE_COLUMN_NUMBER = 3;
+        public const int SAMPLES_COLUMN_NUMBER = 4;
+        public const int COMMENT_COLUMN_NUMBER = 5;
+
+        public const int MAX_GENERATIONS = 100;
+        public const int POPULATION_SIZE = 1000;
+        public const int ELITE_PERCENTAGE = 5;
+        public const double CROSSOVER_PROBABILITY = 0.8;
+        public const double MUTATE_PROBABLITY = 0.1;
+
 
         private static List<SeedsBag> _bags;
         private static int _worst = 0;
@@ -85,30 +108,66 @@ namespace HaZera
             _worst = allseeds * allsamples.Count;
         }
 
+        private int[] CreateSuffledArray(int count)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            int[] arr = new int[count];
+
+            for (int i = 0; i < count; ++i)
+            {
+                arr[i] = i;
+            }
+
+            for (int i = 0; i < count; ++i)
+            {
+                int a = random.Next(0, count - 1);
+                int b = random.Next(0, count - 1);
+                int tmp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = tmp;
+            }
+
+            return arr;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            doWork();
+        }
+
+        private void doWork()
+        {
             int numberOfGenes = _bags.Count;
-            int populationSize = 10;
+            int populationSize = POPULATION_SIZE;
             Population pop = new Population(populationSize);
             for (int i = 0; i < populationSize; ++i)
             {
+                Console.WriteLine("Adding chromosome number " + i);
+                int[] shuffled = CreateSuffledArray(numberOfGenes);
                 Chromosome ch = new Chromosome();
                 for (int g = 0; g < numberOfGenes; g++)
                 {
-                    ch.Genes.Add(new Gene(g));
+                    if (i == 0)
+                    {
+                        ch.Genes.Add(new Gene(g));
+                    }
+                    else
+                    {
+                        ch.Genes.Add(new Gene(shuffled[g]));
+                    }
                 }
-                ch.Genes.Shuffle();
+                //ch.Genes.Shuffle();
                 pop.Solutions.Add(ch);
             }
 
-            Elite elite = new Elite(10);
+            Elite elite = new Elite(ELITE_PERCENTAGE);
 
-            Crossover crossover = new Crossover(0.8)
+            Crossover crossover = new Crossover(CROSSOVER_PROBABILITY)
             {
                 CrossoverType = CrossoverType.DoublePointOrdered
             };
 
-            SwapMutate mutate = new SwapMutate(0.02);
+            SwapMutate mutate = new SwapMutate(MUTATE_PROBABLITY);
 
             GeneticAlgorithm ga = new GeneticAlgorithm(pop, CalculateFitness);
             ga.OnGenerationComplete += ga_OnGenerationComplete;
@@ -117,6 +176,10 @@ namespace HaZera
             ga.Operators.Add(elite);
             ga.Operators.Add(crossover);
             ga.Operators.Add(mutate);
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = MAX_GENERATIONS;
+            progressBar1.Value = 0;
 
             ga.Run(Terminate);
 
@@ -129,11 +192,13 @@ namespace HaZera
             {
                 Console.WriteLine(_bags[(int)gene.RealValue].BagName);
             }
-            CalculateFitness(fittest, true);
+            //CalculateFitness(fittest, true);
+            SaveSolutionToExcel(fittest);
         }
 
         void ga_OnGenerationComplete(object sender, GaEventArgs e)
         {
+            progressBar1.Value = (e.Generation < progressBar1.Maximum)?e.Generation:progressBar1.Maximum;
             Chromosome ch = e.Population.GetTop(1)[0];
             var fitness = CalculateFitness(ch);
             Console.WriteLine("Generation: {0}, Fitness: {1}, chromosome: {2}", e.Generation, ch.Fitness, PrintChromosome(ch));
@@ -149,10 +214,12 @@ namespace HaZera
         public static string PrintChromosome(Chromosome ch)
         {
             string str = "";
+            /*
             foreach (var gene in ch.Genes)
             {
                 str += _bags[(int)gene.RealValue].BagName + ",";
             }
+             */ 
 
             return str;
         }
@@ -164,6 +231,8 @@ namespace HaZera
 
         public static double CalculateFitness(Chromosome ch, bool verbose)
         {
+            //Console.WriteLine("calculating fitness... ");
+
             double fitness = 0.0;
 
             List<Tray> trays = new List<Tray>();
@@ -239,6 +308,7 @@ namespace HaZera
             }
 
             //Console.WriteLine("Fitness: {0} of chromosome {1}", fitness, PrintChromosome(ch));
+            //Console.WriteLine("Fitness: {0} ", fitness);
 
             return fitness;
 
@@ -246,8 +316,110 @@ namespace HaZera
 
         public static bool Terminate(Population population, int currentGeneration, long currentEvaluation)
         {
-            return currentGeneration > 100;
+            return currentGeneration > MAX_GENERATIONS;
         }
 
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm|All Files|*.*";
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                inputFile.Text = dlg.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filename = inputFile.Text;
+
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                return;
+            }
+
+            if (!File.Exists(filename))
+            {
+                return;
+            }
+
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileStream(filename, FileMode.Open, FileAccess.Read));
+            XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(0);
+
+            listView.Items.Clear();
+            _bags.Clear();
+
+            XSSFRow row = null;
+            for (int r = 1; (row = (XSSFRow)sheet.GetRow(r)) != null; ++r)
+            {
+                string strFieldName;
+                XSSFCell cell;
+                cell = (XSSFCell)row.GetCell(FIELD_COLUMN_NUMBER);
+                if (cell.CellType == NPOI.SS.UserModel.CellType.Numeric)
+                {
+                    strFieldName = cell.NumericCellValue.ToString();
+                }
+                else
+                {
+                    strFieldName = cell.StringCellValue;
+                }
+                string strBagName = row.GetCell(BAGNAME_COLUMN_NUMBER).StringCellValue;
+                int nSeedsToPlant = (int)row.GetCell(SEEDSTOPLANT_COLUMN_NUMBER).NumericCellValue;
+                int nSeedsToSample = (int)row.GetCell(SEEDSTOSAMPLE_COLUMN_NUMBER).NumericCellValue;
+
+                //cell = (XSSFCell)row.GetCell(SAMPLES_COLUMN_NUMBER, NPOI.SS.UserModel.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                string strSamples = row.GetCell(SAMPLES_COLUMN_NUMBER, NPOI.SS.UserModel.MissingCellPolicy.CREATE_NULL_AS_BLANK).StringCellValue;
+                string strComment = row.GetCell(COMMENT_COLUMN_NUMBER, NPOI.SS.UserModel.MissingCellPolicy.CREATE_NULL_AS_BLANK).StringCellValue;
+
+                ListViewItem lvi = new ListViewItem(strFieldName);
+                lvi.SubItems.Add(strBagName);
+                lvi.SubItems.Add(nSeedsToPlant.ToString());
+                lvi.SubItems.Add(nSeedsToSample.ToString());
+                lvi.SubItems.Add(strSamples);
+                lvi.SubItems.Add(strComment);
+
+                listView.Items.Add(lvi);
+
+                SeedsBag sb = new SeedsBag(strBagName, strFieldName, nSeedsToPlant, nSeedsToSample, strSamples, strComment);
+                _bags.Add(sb);
+            }
+
+            SaveTestPlan();
+            LoadTestPlan();
+
+        }
+
+        private void SaveSolutionToExcel(Chromosome ch)
+        {
+            string filename = inputFile.Text + ".solution.xlsx";
+            FileStream output = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
+
+            XSSFWorkbook wb = new XSSFWorkbook();
+            XSSFSheet sheet = (XSSFSheet)wb.CreateSheet("Solution");
+            XSSFRow row = (XSSFRow)sheet.CreateRow(0);
+            row.CreateCell(0).SetCellValue("חלקה");
+            row.CreateCell(1).SetCellValue("שקית זרעים");
+            row.CreateCell(2).SetCellValue("זריעה");
+            row.CreateCell(3).SetCellValue("PCR");
+            row.CreateCell(4).SetCellValue("עמידות");
+            row.CreateCell(5).SetCellValue("הערות");
+
+
+            for (int r = 0; r < ch.Genes.Count; ++r)
+            {
+                Gene gene = ch.Genes[r];
+                row = (XSSFRow)sheet.CreateRow(r + 1);
+                SeedsBag sb = _bags[(int)gene.RealValue];
+                row.CreateCell(0).SetCellValue(sb.FieldName);
+                row.CreateCell(1).SetCellValue(sb.BagName);
+                row.CreateCell(2).SetCellValue(sb.SeedsToPlant);
+                row.CreateCell(3).SetCellValue(sb.SeedsToSample);
+                row.CreateCell(4).SetCellValue(string.Join(",", sb.Samples));
+                row.CreateCell(5).SetCellValue(sb.Comment);
+            }
+
+            wb.Write(output);
+            output.Close();
+        }
     }
 }
